a97b6e2bd47b4174a9dd2945a1a2bd38cc15ea9d
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-03-24 09:21:58 +0100

    Add project files.

---
+++ ConsoleApp41/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] städer = new string[3];
+            städer[0] = "Göterborg";
+            städer[1] = "Stockholm";
+            Console.WriteLine("Skriv en stad förutom " + städer[0] + " och " + städer[1]);
+            städer[2] = Console.ReadLine();
+            Console.WriteLine($"De tre städerna är {städer[0]}, {städer[1]} och {städer[2]}");
+        }
+
+    }
+
+}
